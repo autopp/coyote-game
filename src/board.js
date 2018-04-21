d03447def4edd1c9e03ba5180622be424cbda5d5
autopp <autopp.inc@gmail.com>
2018-04-21 11:38:50 +0900

    feat: show players card

--- src/board.js
+++ src/board.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 export class CoyoteBoard extends React.Component {
   render() {
+    let playerCards = [];
+    for (let playerID in this.props.G.players) {
+      playerCards.push(<div key={playerID}>player {playerID}: {this.props.G.players[playerID].card}</div>)
+    }
+    let message = <div>current player: {this.props.ctx.currentPlayer}</div>
     return (
-      <div>hello world</div>
+      <div>
+        {playerCards}
+        {message}
+      </div>
     );
   }
 }
