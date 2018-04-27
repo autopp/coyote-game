6013c1cf40a4a47717897fba1b7ffd7b8339c122
autopp <autopp.inc@gmail.com>
2018-04-27 09:48:31 +0900

    fix: disable multi player mode

--- src/index.js
+++ src/index.js
@@ -4,18 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-let playerID="";
-let gameID="default";
-
-window.location.search.substring(1).split('&').forEach((kv) => {
-  let [k, v] = kv.split('=');
-  if (k === "playerID") {
-    playerID = v;
-  }
-  if (k === "gameID") {
-    gameID = v;
-  }
-});
-
-ReactDOM.render(<App gameID={gameID} playerID={playerID} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
